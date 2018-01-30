35aa7895483eaad13ead138157e6c3697bdf3926
Cory Hoban <hobancor@gmail.com>
2018-01-30 16:40:55 -0500

    Add Token Authentication through signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,17 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id , iat: timestamp}, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (!email || !pasword) {
+  if (!email || !password) {
     return res.status(422).send({error: 'You must provide email and password'});
   }
   // See if a user with a the given email exists
@@ -26,7 +33,7 @@ exports.signup = function(req, res, next) {
       if (err) { return next(err); }
 
       //Respond to request indicationg the user was created
-      res.json({success: true});
+      res.json({ token: tokenForUser(user) });
     });
 
   });
