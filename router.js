1c1a36212389219a89087a1c991e83bd5d23d6ff
Cory Hoban <hobancor@gmail.com>
2018-02-05 10:28:17 -0500

    reverting to before heroku

--- router.js
+++ router.js
@@ -24,13 +24,4 @@ module.exports = function(app) {
       res.send(e);
     });
   });
-
-  app.get('/todos', (req, res) => {
-    Todo.find()
-      .then((todos) => {
-        res.send({todos});
-      }, (e) => {
-        res.status(402).send(e)
-      });
-  });
 }
