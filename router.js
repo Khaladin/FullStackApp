c2914b7acb7d3ea22f85268d838a5aa15593d3d9
Cory Hoban <hobancor@gmail.com>
2018-02-28 12:34:15 -0500

    Adjusted GET :ID route

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const Authentication = require('./controllers/authentication');
 const passportService = require('./services/passport');
 const passport = require('passport');
+const {ObjectID} = require('mongodb');
 const {Todo} = require('./models/todo');
 
 const requireAuth = passport.authenticate('jwt', {session: false});
@@ -12,6 +13,7 @@ module.exports = function(app) {
   });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
+
   app.post('/newpost', (req, res) => {
     var todo = new Todo({
       title: req.body.title,
@@ -24,6 +26,7 @@ module.exports = function(app) {
       res.send(e);
     });
   });
+
   app.get('/todos', (req, res) => {
     Todo.find()
       .then((todos) => {
@@ -31,5 +34,33 @@ module.exports = function(app) {
       }, (e) => {
         res.status(402).send(e)
       });
+  });
+
+  app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send('not valid objectID');
+    }
+
+    Todo.findOne().then((todo) => {
+        res.send({todo});
+    }, (e) => {
+      res.status(404).send(e.response)
+    });
+  });
+
+  app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    Todo.findOneAndRemove({
+      _id:id,
+    }).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.status(200).send({todo});
+    }).catch((e) => res.send(e));
   });
 }
